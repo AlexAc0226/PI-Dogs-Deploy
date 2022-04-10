b689dae0eae6a5a47d7acdea3473412752c4b6aa
AlexAc0226 <88632261+AlexAc0226@users.noreply.github.com>
2022-04-10 02:39:11 -0300

    Deploy cuatro

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
-// import all routers;
-const productRouter = require('./product.js');
-
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const dogsRouter = require('./dog.js')
+const temperamentRouter = require('./temperament.js')
 
 const router = Router();
 
-// load each router on a route
-// i.e: router.use('/auth', authRouter);
-// router.use('/auth', authRouter);
-router.use('/products', productRouter);
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+router.use('/dogs', dogsRouter)
+router.use('/temperament', temperamentRouter)
 
 module.exports = router;
